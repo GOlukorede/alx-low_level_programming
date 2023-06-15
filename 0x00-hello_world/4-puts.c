3b6a03ad22b5c7847666b82d6393ada038d6d696
GOlukorede <samsongreats@gmail.com>
2023-06-15 16:13:13 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *Program that prints something
+ * My first C program
+ *
+ * Program that prints a meaasage on programming
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
+
