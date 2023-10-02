edd7c19ca39048694094918b1238a7f6572c7565
GOlukorede <samsongreats@gmail.com>
2023-10-02 06:29:27 -0700

    append file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,24 +11,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, wr, len = 0;
+	int o, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	if (!text_content)
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	wr = write(fd, text_content, len);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-	if (fd == -1 || wr == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(fd);
+	close(o);
 
 	return (1);
 }
