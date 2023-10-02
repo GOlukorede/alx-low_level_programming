a469fae8738f3acb2ad37043f0e8cabe49a7effe
GOlukorede <samsongreats@gmail.com>
2023-10-02 06:26:53 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,24 +8,23 @@
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr, len = 0;
+	int fd, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	if (!text_content)
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = write(fd, text_content, len);
+	w = write(fd, text_content, len);
 
-	if (fd == -1 || wr == -1)
+	if (fd == -1 || w == -1)
 		return (-1);
 
 	close(fd);
